eda3d7385ee3852ecf74e70345e6c911e1a820cb
Kegan Maher <kegan.maher@smgov.net>
2019-02-21 13:04:23 -0800

    explicit date_format

--- mds/api/client.py
+++ mds/api/client.py
@@ -26,7 +26,7 @@ class ProviderClient(OAuthClientCredentialsAuth):
             - git tag
         """
         self.providers = providers if providers is not None else get_registry(ref)
-        self.encoder = CustomJsonEncoder()
+        self.encoder = CustomJsonEncoder(date_format="unix")
 
     def _auth_session(self, provider):
         """
